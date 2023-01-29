a622c77d89f52c7212f196dff83a171f079b9c7a
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-01-30 00:15:21 +0100

    Added articles data

--- main.py
+++ main.py
@@ -21,7 +21,28 @@ def make_dataset():
     raw_data = get_raw_data()
     articles = raw_data.find_all("article")
     for article in articles:
-        article_data = {"titre": article.find("a", {"class": "cept-tt thread-link linkPlain thread-title--list js-thread-title"}).get("title")}
+        if article.find("span", {"class": "size--all-s text--color-grey space--l-1 space--r-2 cept-show-expired-threads hide--toW3"}):
+            expired = True
+            score =   article.find("span", {"class": "space--h-2 text--b"})
+            price =   article.find("span", {"class": "thread-price text--b cept-tp size--all-l size--fromW3-xl text--color-greyShade"})
+        else:
+            expired =  False
+            score =    article.find("span", {"class": "cept-vote-temp vote-temp vote-temp--hot"})
+            price =    article.find("span", {"class": "thread-price text--b cept-tp size--all-l size--fromW3-xl"})
+        title =         article.find("a", {"class": "cept-tt thread-link linkPlain thread-title--list js-thread-title"})
+        datetime =      article.find("span", {"class": "hide--toW3"})
+        former_price =  article.find("span", {"class": "mute--text text--lineThrough size--all-l size--fromW3-xl"})
+        discount =      article.find("span", {"class": "space--ml-1 size--all-l size--fromW3-xl"})
+
+        article_data = {
+            "titre":        title.get("title"),
+            "expired":      expired,
+            "score":        int(score.text.replace("\xb0", "")),
+            "datetime":     datetime.text,
+            "price":        price.text,
+            "former_price": former_price.text if former_price else None,
+            "discount":     discount.text if discount else None
+        }
         dataset.append(article_data)
 
     with open(f"{os.getcwd()}/output/output_data.json", "w") as file:
